6269523912b6b6f10ec1a300dce64cd038a06306
SahaRahul <rahulsaha86@gmail.com>
2019-05-27 19:10:38 -0700

    v0.2

--- BasicProg2.py
+++ BasicProg2.py
@@ -34,3 +34,37 @@ diamondData7.shape
 diamondData8 = diamondData6[(diamondData6['color'] == "E") | 
                             (diamondData6['color'] == "I")]
 diamondData8.shape
+
+class display(object):
+    """Display HTML representation of multiple objects"""
+    template = """<div style="float: left; padding: 10px;">
+    <p style='font-family:"Courier New", Courier, monospace'>{0}</p>{1}
+    </div>"""
+    def __init__(self, *args):
+        self.args = args
+        
+    def _repr_html_(self):
+        return '\n'.join(self.template.format(a, eval(a)._repr_html_())
+                         for a in self.args)
+    
+    def __repr__(self):
+        return '\n\n'.join(a + '\n' + repr(eval(a))
+                           for a in self.args)
+
+df1 = pd.DataFrame({'employee': ['Bob', 'Jake', 'Lisa', 'Sue', 'Rahul'],
+                    'group': ['Accounting', 'Engineering', 'Engineering', 'HR', 'Analytics']})
+df2 = pd.DataFrame({'employee': ['Lisa', 'Bob', 'Jake', 'Sue', 'Sudipta'],
+                    'hire_date': [2004, 2008, 2012, 2014, 2018]})
+display('df1', 'df2')
+
+df1_df2merge = pd.merge(df1, df2, on = 'employee', how = 'inner' )
+df1_df2merge
+
+df1_df2merge1 = pd.merge(df1, df2, on = 'employee', how = 'left' )
+df1_df2merge1
+
+df1_df2merge2 = pd.merge(df1, df2, on = 'employee', how = 'outer' )
+df1_df2merge2
+
+df1_df2merge1.to_csv('~/git/SahaRahul/practicePython/leftjoin.csv')
+df1_df2merge2.to_csv('~/git/SahaRahul/practicePython/fullouter.csv')
